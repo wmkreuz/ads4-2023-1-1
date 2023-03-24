78fe19d32991a578db2a3157fb5d0cd4ac7dbc1b
Willhan Kreuz <willhan.kreuz@santerra.local>
2023-03-23 21:35:18 -0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'valida-dominio';
+  public email:string = '';
+
+  public borda_email:string = '';
+
+  salvar():boolean {
+    if (this.email != '' && !this.isValidDomain(this.email)){
+      this.borda_email = 'border:1px solid #FF0000';
+      return false;
+    }
+
+    return true;
+  }
+
+  isValidDomain(email:string):boolean{
+    let split = email.split('@');
+    let domain:string = split[1];
+
+    if (domain != "alunos.sc.senac.br"){
+      return false;
+    }
+    return true;
+  }
 }
